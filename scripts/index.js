a54e9aa4fe1f9c70055d0aed7c23fae6887d8113
Pranav tiwari <67794060+Pranav-8bit@users.noreply.github.com>
2022-08-30 14:09:23 +0530

    minor Bug fix3.0

--- scripts/index.js
+++ scripts/index.js
@@ -142,7 +142,8 @@ const deleteTask = (e) => {
 };
 
 const editTask = (e) => {
-  if (!e) e = windows.event;
+  if (!e) e = window.event;
+
   const targetID = e.target.id;
   const type = e.target.tagName;
 
@@ -167,27 +168,30 @@ const editTask = (e) => {
   taskDescription.setAttribute("contenteditable", "true");
   taskType.setAttribute("contenteditable", "true");
 
-  submitButton.setAttribute("onclick", "saveEdit.apply(this, argumnents)");
+  submitButton.setAttribute("onclick", "saveEdit.apply(this, arguments)");
   submitButton.removeAttribute("data-bs-toggle");
   submitButton.removeAttribute("data-bs-target");
   submitButton.innerHTML = "Save Changes";
 };
 
 const saveEdit = (e) => {
-  if (!e) e = windows.event;
+  if (!e) e = window.event;
+
   const targetID = e.target.id;
   const parentNode = e.target.parentNode.parentNode;
+  // console.log(parentNode.childNodes);
 
-  taskTitle = parentNode.childNodes[3].childNodes[3];
-  taskDescription = parentNode.childNodes[3].childNodes[5];
-  taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
-  submitButton = parentNode.childNodes[5].childNodes[1];
+  const taskTitle = parentNode.childNodes[3].childNodes[3];
+  const taskDescription = parentNode.childNodes[3].childNodes[5];
+  const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+  const submitButton = parentNode.childNodes[5].childNodes[1];
 
   const updateData = {
     taskTitle: taskTitle.innerHTML,
     taskDescription: taskDescription.innerHTML,
     taskType: taskType.innerHTML,
   };
+
   let stateCopy = state.taskList;
 
   stateCopy = stateCopy.map((task) =>
@@ -209,10 +213,9 @@ const saveEdit = (e) => {
   taskDescription.setAttribute("contenteditable", "false");
   taskType.setAttribute("contenteditable", "false");
 
-  submitButton.setAttribute("onclick", "openTask.apply(this,arguments)");
+  submitButton.setAttribute("onclick", "openTask.apply(this, arguments)");
   submitButton.setAttribute("data-bs-toggle", "modal");
   submitButton.setAttribute("data-bs-target", "#showTask");
-
   submitButton.innerHTML = "Open Task";
 };
 
